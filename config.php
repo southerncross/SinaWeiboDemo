a6cd3cd4895adce463dc00531ae53a28a7797ca9
southerncross <southerncross@pku.edu.cn>
2013-04-08 14:45:40 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+header('Content-Type: text/html; charset=UTF-8');
+
+define("WB_AKEY" , '2262370750');
+define("WB_SKEY" , '29ec438f7f57b58435230c689c51e1bf');
+define("WB_CALLBACK_URL" , 'http://localhost/callback.php');
+
+?>
