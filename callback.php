e25d9c65673d6a64dfbe31e0cc2bbe51009da2ff
southerncross <southerncross@pku.edu.cn>
2013-04-09 17:11:46 +0800

    Update callback.php

--- callback.php
+++ callback.php
@@ -20,7 +20,7 @@ if ($token) {
 	$_SESSION['token'] = $token;
 	setcookie( 'weibojs_'.$o->client_id, http_build_query($token) );
 ?>
-授权完成！
+授权完成！<a href="weibolist.php">进入你的微博列表页面</a><br />
 <?php
 } else {
 ?>
